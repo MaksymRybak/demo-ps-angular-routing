bd4bf202db3be9c496716ac86f2a647f28d6ec90
Maksym Rybak <rybak.maksym@gmail.com>
2019-11-28 21:53:38 +0100

    secondary route demo

--- src/app/messages/message.module.ts
+++ src/app/messages/message.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 import { MessageComponent } from './message.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path: 'message',
+        component: MessageComponent,
+        outlet: 'popup'
+      }
+    ])
   ],
   declarations: [
     MessageComponent
