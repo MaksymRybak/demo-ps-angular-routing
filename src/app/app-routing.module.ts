d599b0fbe1d0cb944236f0dbd53f072e66232bd6
Maksym Rybak <rybak.maksym@gmail.com>
2019-12-15 15:36:35 +0100

    set preloadStrategy to PreloadAllModules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
@@ -8,7 +8,7 @@ const ROUTES = [
   { path: 'welcome', component: WelcomeComponent },
   {
     path: 'products',
-    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
     loadChildren: () => import('./products/product.module').then(m => m.ProductModule)
   },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
@@ -17,7 +17,7 @@ const ROUTES = [
 
 @NgModule({
   imports: [
-     RouterModule.forRoot(ROUTES, { enableTracing: true })
+     RouterModule.forRoot(ROUTES, { enableTracing: true, preloadingStrategy: PreloadAllModules })
   ],
   exports: [
     RouterModule
